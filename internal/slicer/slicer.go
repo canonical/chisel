0c8d54013487bbcd09c4f27f7f451d709adf2306
Tomáš Virtus <tomas.virtus@canonical.com>
2022-10-25 18:14:59 +0200

    slicer: Build and lookup known paths

--- internal/slicer/slicer.go
+++ internal/slicer/slicer.go
@@ -15,7 +15,6 @@ import (
 	"github.com/canonical/chisel/internal/fsutil"
 	"github.com/canonical/chisel/internal/scripts"
 	"github.com/canonical/chisel/internal/setup"
-	"github.com/canonical/chisel/internal/strdist"
 )
 
 type RunOptions struct {
@@ -29,6 +28,23 @@ func Run(options *RunOptions) error {
 	archives := make(map[string]archive.Archive)
 	extract := make(map[string]map[string][]deb.ExtractInfo)
 	pathInfos := make(map[string]setup.PathInfo)
+	knownPaths := make(map[string]bool)
+
+	knownPaths["/"] = true
+
+	addKnownPath := func(path string) {
+		path = filepath.Clean(path)
+		if path[0] != '/' {
+			panic("bug: tried to add relative path to known paths")
+		}
+		for path != "/" {
+			if _, exists := knownPaths[path]; exists {
+				break
+			}
+			knownPaths[path] = true
+			path = filepath.Dir(path)
+		}
+	}
 
 	oldUmask := syscall.Umask(0)
 	defer func() {
@@ -72,6 +88,7 @@ func Run(options *RunOptions) error {
 			if len(pathInfo.Arch) > 0 && !contains(pathInfo.Arch, arch) {
 				continue
 			}
+			addKnownPath(targetPath)
 			pathInfos[targetPath] = pathInfo
 			if pathInfo.Kind == setup.CopyPath || pathInfo.Kind == setup.GlobPath {
 				sourcePath := pathInfo.Info
@@ -138,6 +155,12 @@ func Run(options *RunOptions) error {
 		}
 	}
 
+	for _, expandedPaths := range globbedPaths {
+		for _, path := range expandedPaths {
+			addKnownPath(path)
+		}
+	}
+
 	// Create new content not coming from packages.
 	done := make(map[string]bool)
 	for _, slice := range options.Selection.Slices {
@@ -198,12 +221,8 @@ func Run(options *RunOptions) error {
 		return nil
 	}
 	checkRead := func(_ *scripts.ContentValue, path string) error {
-		if _, ok := pathInfos[path]; !ok {
-			for globPath := range globbedPaths {
-				if strdist.GlobPath(globPath, path) {
-					return nil
-				}
-			}
+		path = filepath.Clean(path)
+		if !knownPaths[path] {
 			return fmt.Errorf("cannot read file which is not selected: %s", path)
 		}
 		return nil
