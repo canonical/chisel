0135df0d6651c8d55327c3bd153a19cea3b06609
Ed Jones <ed.jones@canonical.com>
2023-01-18 18:42:03 +0000

    Fix `Reindent` panicking on valid, unindented inputs

--- internal/testutil/reindent.go
+++ internal/testutil/reindent.go
@@ -10,19 +10,21 @@ import (
 func Reindent(in string) []byte {
 	var buf bytes.Buffer
 	var trim string
+	var trimSet bool
 	for _, line := range strings.Split(in, "\n") {
-		if trim == "" {
+		if !trimSet {
 			trimmed := strings.TrimLeft(line, "\t")
 			if trimmed == "" {
 				continue
 			}
-			if trimmed[0] == ' ' {
+			if len(trimmed) != len(line) && trimmed[0] == ' ' {
 				panic("Tabs and spaces mixed early on string:\n" + in)
 			}
 			trim = line[:len(line)-len(trimmed)]
+			trimSet = true
 		}
 		trimmed := strings.TrimPrefix(line, trim)
-		if len(trimmed) == len(line) && strings.Trim(line, "\t ") != "" {
+		if len(trimmed) == len(line) && trim != "" && strings.Trim(line, "\t ") != "" {
 			panic("Line not indented consistently:\n" + line)
 		}
 		trimmed = strings.ReplaceAll(trimmed, "\t", "    ")
