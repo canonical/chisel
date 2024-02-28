f4557ecfe7c42cd17edf0c868ebe11362746e528
Alberto Carretero <angelalbertoc.r@gmail.com>
2024-02-28 14:19:45 +0100

    explicit test for nil Create closure

--- internal/deb/extract_test.go
+++ internal/deb/extract_test.go
@@ -17,6 +17,7 @@ type extractTest struct {
 	summary string
 	pkgdata []byte
 	options deb.ExtractOptions
+	hackopt func(o *deb.ExtractOptions)
 	globbed map[string][]string
 	result  map[string]string
 	// paths which the extractor created explicitly.
@@ -78,7 +79,43 @@ var extractTests = []extractTest{{
 		"/usr/share/doc",
 	},
 }, {
-
+	summary: "Extract a few entries, nil Create closure",
+	pkgdata: testutil.PackageData["base-files"],
+	options: deb.ExtractOptions{
+		Extract: map[string][]deb.ExtractInfo{
+			"/usr/bin/hello": []deb.ExtractInfo{{
+				Path: "/usr/bin/hello",
+			}},
+			"/etc/os-release": []deb.ExtractInfo{{
+				Path: "/etc/os-release",
+			}},
+			"/usr/lib/os-release": []deb.ExtractInfo{{
+				Path: "/usr/lib/os-release",
+			}},
+			"/usr/share/doc/": []deb.ExtractInfo{{
+				Path: "/usr/share/doc/",
+			}},
+			"/tmp/": []deb.ExtractInfo{{
+				Path: "/tmp/",
+			}},
+		},
+	},
+	result: map[string]string{
+		"/tmp/":               "dir 01777",
+		"/usr/":               "dir 0755",
+		"/usr/bin/":           "dir 0755",
+		"/usr/bin/hello":      "file 0775 eaf29575",
+		"/usr/share/":         "dir 0755",
+		"/usr/share/doc/":     "dir 0755",
+		"/usr/lib/":           "dir 0755",
+		"/usr/lib/os-release": "file 0644 ec6fae43",
+		"/etc/":               "dir 0755",
+		"/etc/os-release":     "symlink ../usr/lib/os-release",
+	},
+	hackopt: func(o *deb.ExtractOptions) {
+		o.Create = nil
+	},
+}, {
 	summary: "Copy a couple of entries elsewhere",
 	pkgdata: testutil.PackageData["base-files"],
 	options: deb.ExtractOptions{
@@ -102,7 +139,6 @@ var extractTests = []extractTest{{
 	},
 	createdPaths: []string{"/usr", "/usr/foo/bin/hello-2", "/usr/other"},
 }, {
-
 	summary: "Copy same file twice",
 	pkgdata: testutil.PackageData["base-files"],
 	options: deb.ExtractOptions{
@@ -327,6 +363,10 @@ func (s *S) TestExtract(c *C) {
 			createdPaths = append(createdPaths, relPath)
 			_, err := fsutil.Create(o)
 			return err
+		}
+
+		if test.hackopt != nil {
+			test.hackopt(&options)
 		}
 
 		if test.globbed != nil {
