af5826443061eb617d7dd23a4a2d70890a429cd6
Rafid Bin Mostofa <rafid.mostofa@canonical.com>
2024-11-25 21:24:40 +0600

    Revert "test: only add tests for GeneratePath"
    This reverts commit 167a6ebc5ea5a8742fb0a3dce39c41022c8c9608.

--- internal/setup/export_test.go
+++ internal/setup/export_test.go
@@ -1,3 +1,4 @@
 package setup
 
 type YAMLPath = yamlPath
+type YAMLArch = yamlArch
--- internal/setup/setup_test.go
+++ internal/setup/setup_test.go
@@ -2241,13 +2241,93 @@ func (s *S) TestParseSliceKey(c *C) {
 	}
 }
 
-func (s *S) TestGeneratePathSameContent(c *C) {
-	path1 := &setup.YAMLPath{Generate: setup.GenerateManifest}
-	path2 := &setup.YAMLPath{Generate: setup.GenerateManifest}
-	result := path1.SameContent(path2)
-	c.Assert(result, Equals, true)
+var sampleText = "sample"
 
-	path2.Generate = setup.GenerateNone
-	result = path1.SameContent(path2)
-	c.Assert(result, Equals, false)
+var yamlPathTests = []struct {
+	summary      string
+	path1, path2 *setup.YAMLPath
+	result       bool
+}{{
+	summary: "Text path",
+	path1:   &setup.YAMLPath{Text: &sampleText, Mutable: true, Mode: 0644},
+	path2:   &setup.YAMLPath{Text: &sampleText, Mutable: true, Mode: 0644},
+	result:  true,
+}, {
+	summary: "Dir path",
+	path1:   &setup.YAMLPath{Dir: true, Mode: 0755},
+	path2:   &setup.YAMLPath{Dir: true, Mode: 0755},
+	result:  true,
+}, {
+	summary: "Symlink path",
+	path1:   &setup.YAMLPath{Symlink: "foo"},
+	path2:   &setup.YAMLPath{Symlink: "foo"},
+	result:  true,
+}, {
+	summary: "Generate path",
+	path1:   &setup.YAMLPath{Generate: setup.GenerateManifest},
+	path2:   &setup.YAMLPath{Generate: setup.GenerateManifest},
+	result:  true,
+}, {
+	summary: "Copy path",
+	path1:   &setup.YAMLPath{Copy: "foo"},
+	path2:   &setup.YAMLPath{Copy: "foo"},
+	result:  true,
+}, {
+	summary: `"arch" is not checked`,
+	path1:   &setup.YAMLPath{Text: nil, Arch: setup.YAMLArch{List: []string{"amd64"}}},
+	path2:   &setup.YAMLPath{Text: nil, Arch: setup.YAMLArch{List: []string{"arm64"}}},
+	result:  true,
+}, {
+	summary: `"until" is not checked`,
+	path1:   &setup.YAMLPath{Copy: "foo", Mutable: true, Until: setup.UntilMutate},
+	path2:   &setup.YAMLPath{Copy: "foo", Mutable: true},
+	result:  true,
+}, {
+	summary: `Empty "text" produces nil pointer`,
+	path1:   &setup.YAMLPath{Text: nil},
+	path2:   &setup.YAMLPath{Text: nil},
+	result:  true,
+}, {
+	summary: `Different "generate" values`,
+	path1:   &setup.YAMLPath{Generate: setup.GenerateManifest},
+	path2:   &setup.YAMLPath{Generate: setup.GenerateNone},
+	result:  false,
+}, {
+	summary: `Different "text" pointers`,
+	path1:   &setup.YAMLPath{Text: new(string)},
+	path2:   &setup.YAMLPath{Text: new(string)},
+	result:  false,
+}, {
+	summary: `Different "copy" values`,
+	path1:   &setup.YAMLPath{Copy: "foo"},
+	path2:   &setup.YAMLPath{Copy: "bar"},
+	result:  false,
+}, {
+	summary: `Different "make" (Dir) values`,
+	path1:   &setup.YAMLPath{Dir: true},
+	path2:   &setup.YAMLPath{Dir: false},
+	result:  false,
+}, {
+	summary: `Different "symlink" values`,
+	path1:   &setup.YAMLPath{Symlink: "foo"},
+	path2:   &setup.YAMLPath{Symlink: "bar"},
+	result:  false,
+}, {
+	summary: `Different "mode" values`,
+	path1:   &setup.YAMLPath{Text: nil, Mode: 0644},
+	path2:   &setup.YAMLPath{Text: nil, Mode: 0755},
+	result:  false,
+}, {
+	summary: `Different "mutable" values`,
+	path1:   &setup.YAMLPath{Text: nil, Mutable: true},
+	path2:   &setup.YAMLPath{Text: nil, Mutable: false},
+	result:  false,
+}}
+
+func (s *S) TestYAMLPathSameContent(c *C) {
+	for _, test := range yamlPathTests {
+		c.Logf("Summary: %s", test.summary)
+		result := test.path1.SameContent(test.path2)
+		c.Assert(result, Equals, test.result)
+	}
 }
