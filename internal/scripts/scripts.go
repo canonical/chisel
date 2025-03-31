3b4879976e4fde36ddb4e97470c92d38da39a9d5
Nils de Bruin <nils@debruinmail.nl>
2025-03-31 15:45:40 +0200

    fix: further linting fixes

--- internal/scripts/scripts.go
+++ internal/scripts/scripts.go
@@ -7,16 +7,11 @@ import (
 	"path/filepath"
 	"strings"
 
-	"go.starlark.net/resolve"
 	"go.starlark.net/starlark"
 	"go.starlark.net/syntax"
 
 	"github.com/canonical/chisel/internal/fsutil"
 )
-
-func init() {
-	resolve.AllowGlobalReassign = true
-}
 
 type Value = starlark.Value
 
@@ -28,7 +23,9 @@ type RunOptions struct {
 
 func Run(opts *RunOptions) error {
 	thread := &starlark.Thread{Name: opts.Label}
-	fileOptions := &syntax.FileOptions{}
+	fileOptions := &syntax.FileOptions{
+		GlobalReassign: true,
+	}
 	globals, err := starlark.ExecFileOptions(fileOptions, thread, opts.Label, opts.Script, opts.Namespace)
 	_ = globals
 	return err
