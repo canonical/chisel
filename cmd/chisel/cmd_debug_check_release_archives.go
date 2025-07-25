ca37d0a9594c04e570ac2cb67efa47562812260f
Alberto Carretero <angelalbertoc.r@gmail.com>
2025-07-25 17:56:10 +0200

    feat: debug command to check release archives (#229)
    Introduce the chisel debug check-release-archives command
    that verifies that Chisel assumptions avoid the archives hold. For
    example, Chisel assumes that parent directories bundled in several
    packages in the archive are consistent with one another (i.e. have the
    same mode). If the is not the case, we need to flag the issue and
    possibly handle it in the slice definitions files.

---
+++ cmd/chisel/cmd_debug_check_release_archives.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"archive/tar"
+	"errors"
+	"fmt"
+	"io"
+	"slices"
+	"strings"
+
+	"github.com/jessevdk/go-flags"
+	"gopkg.in/yaml.v3"
+
+	"github.com/canonical/chisel/internal/archive"
+	"github.com/canonical/chisel/internal/cache"
+	"github.com/canonical/chisel/internal/deb"
+	"github.com/canonical/chisel/internal/setup"
+)
+
+var shortCheckReleaseArchivesHelp = "Check the release's archives"
+
+var longCheckReleaseArchivesHelp = `
+The check-release-archives command downloads all the packages for a given
+release to check that there are no issues which are not handled in the slice
+definition files (SDFs).
+
+Types of issues:
+- "path-conflict". When multiple packages provide different content for the
+same path. For example, for parent directories which are not listed explicitly
+in the SDFs, Chisel will try to preserve permissions by using the mode from the
+package's tarball. If several packages have different permissions for the same
+directory, that could lead to a conflict.
+`
+
+var checkReleaseArchivesDescs = map[string]string{
+	"release": "Chisel release name or directory (e.g. ubuntu-22.04)",
+	"arch":    "Package architecture",
+}
+
+type cmdDebugCheckReleaseArchives struct {
+	Release string `long:"release" value-name:"<branch|dir>"`
+	Arch    string `long:"arch" value-name:"<arch>"`
+}
+
+func init() {
+	addDebugCommand("check-release-archives", shortCheckReleaseArchivesHelp, longCheckReleaseArchivesHelp, func() flags.Commander { return &cmdDebugCheckReleaseArchives{} }, checkReleaseArchivesDescs, nil)
+}
+
+var archiveOpen = archive.Open
+
+type pathObservation struct {
+	Archive  string   `yaml:"archive"`
+	Packages []string `yaml:"packages,flow"`
+	Kind     string   `yaml:"kind"`
+	Mode     yamlMode `yaml:"mode,omitempty"`
+	Link     string   `yaml:"link,omitempty"`
+}
+
+func (cmd *cmdDebugCheckReleaseArchives) Execute(args []string) error {
+	release, err := obtainRelease(cmd.Release)
+	if err != nil {
+		return err
+	}
+
+	archives := make(map[string]archive.Archive)
+	for archiveName, archiveInfo := range release.Archives {
+		openArchive, err := archiveOpen(&archive.Options{
+			Label:      archiveName,
+			Version:    archiveInfo.Version,
+			Arch:       cmd.Arch,
+			Suites:     archiveInfo.Suites,
+			Components: archiveInfo.Components,
+			Pro:        archiveInfo.Pro,
+			CacheDir:   cache.DefaultDir("chisel"),
+			PubKeys:    archiveInfo.PubKeys,
+		})
+		if err == archive.ErrCredentialsNotFound {
+			logf("Archive %q ignored: credentials not found\n", archiveName)
+			continue
+		} else if err != nil {
+			return err
+		}
+		archives[archiveName] = openArchive
+	}
+
+	pathObs, err := computePathObservations(release, archives)
+	if err != nil {
+		return err
+	}
+
+	var issues []any
+	type pathConflict struct {
+		Issue        string            `yaml:"issue"`
+		Path         string            `yaml:"path"`
+		Observations []pathObservation `yaml:"observations"`
+	}
+	var sortedPaths []string
+	for path := range pathObs {
+		sortedPaths = append(sortedPaths, path)
+	}
+	slices.Sort(sortedPaths)
+	for _, path := range sortedPaths {
+		observations := pathObs[path]
+		if hasPathConflict(observations) {
+			issues = append(issues, pathConflict{
+				// At this time, there is only one possible type of conflict,
+				// we do not need to check.
+				Issue:        "path-conflict",
+				Path:         path,
+				Observations: observations,
+			})
+		}
+	}
+
+	if len(issues) > 0 {
+		err := yaml.NewEncoder(Stdout).Encode(issues)
+		if err != nil {
+			return fmt.Errorf("internal error: cannot marshal issue list: %s", err)
+		}
+		return errors.New("issues found in the release archives")
+	}
+
+	return nil
+}
+
+func computePathObservations(release *setup.Release, archives map[string]archive.Archive) (map[string][]pathObservation, error) {
+	var orderedPkgs []string
+	for packageName := range release.Packages {
+		orderedPkgs = append(orderedPkgs, packageName)
+	}
+	slices.Sort(orderedPkgs)
+	var orderedArchives []string
+	for archiveName := range archives {
+		orderedArchives = append(orderedArchives, archiveName)
+	}
+	slices.Sort(orderedArchives)
+
+	pathObs := map[string][]pathObservation{}
+	for _, archiveName := range orderedArchives {
+		archive := archives[archiveName]
+		logf("Processing archive %s...", archiveName)
+		for _, pkgName := range orderedPkgs {
+			if !archive.Exists(pkgName) {
+				continue
+			}
+			pkgReader, _, err := archive.Fetch(pkgName)
+			if err != nil {
+				return nil, err
+			}
+			dataReader, err := deb.DataReader(pkgReader)
+			if err != nil {
+				return nil, err
+			}
+			tarReader := tar.NewReader(dataReader)
+			for {
+				tarHeader, err := tarReader.Next()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					return nil, err
+				}
+
+				path, ok := sanitizeTarPath(tarHeader.Name)
+				if !ok {
+					continue
+				}
+				if tarHeader.FileInfo().Mode().IsRegular() {
+					continue
+				}
+
+				// Make paths uniform: while directories always end in '/',
+				// symlinks don't.
+				path = strings.TrimSuffix(path, "/")
+
+				// We look for a previous observation that extracts the same
+				// content in terms of mode, link, etc. and we add the package
+				// to it. If there is none, we create a new one.
+				observations := pathObs[path]
+				index := slices.IndexFunc(observations, func(o pathObservation) bool {
+					return o.Archive == archiveName &&
+						tarHeader.Linkname == o.Link &&
+						tarHeader.Mode == int64(o.Mode)
+				})
+				if index != -1 {
+					observations[index].Packages = append(observations[index].Packages, pkgName)
+				} else {
+					kind := "symlink"
+					if tarHeader.FileInfo().IsDir() {
+						kind = "dir"
+					}
+					var mode yamlMode
+					if kind == "dir" {
+						mode = yamlMode(tarHeader.Mode)
+					}
+					pathObs[path] = append(pathObs[path], pathObservation{
+						Kind:     kind,
+						Mode:     mode,
+						Link:     tarHeader.Linkname,
+						Archive:  archiveName,
+						Packages: []string{pkgName},
+					})
+				}
+			}
+		}
+	}
+	return pathObs, nil
+}
+
+func hasPathConflict(observations []pathObservation) bool {
+	if len(observations) == 0 {
+		return false
+	}
+
+	base := observations[0]
+	for _, observation := range observations[1:] {
+		if observation.Kind != base.Kind ||
+			observation.Mode != base.Mode ||
+			observation.Link != base.Link {
+			return true
+		}
+	}
+	return false
+}
+
+// sanitizeTarPath removes the leading "./" from the source path in the tarball,
+// and verifies that the path is not empty.
+func sanitizeTarPath(path string) (string, bool) {
+	if len(path) < 3 || path[0] != '.' || path[1] != '/' {
+		return "", false
+	}
+	return path[1:], true
+}
+
+type yamlMode int64
+
+func (ym yamlMode) MarshalYAML() (interface{}, error) {
+	// Workaround for marshalling integers in octal format.
+	// Ref: https://github.com/go-yaml/yaml/issues/420.
+	node := &yaml.Node{}
+	err := node.Encode(uint(ym))
+	if err != nil {
+		return nil, err
+	}
+	node.Value = fmt.Sprintf("0%o", ym)
+	return node, nil
+}
+
+var _ yaml.Marshaler = yamlMode(0)
---
+++ cmd/chisel/cmd_debug_check_release_archives_test.go
@@ -0,0 +1,331 @@
+package main_test
+
+import (
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	. "gopkg.in/check.v1"
+	"gopkg.in/yaml.v3"
+
+	chisel "github.com/canonical/chisel/cmd/chisel"
+	"github.com/canonical/chisel/internal/archive"
+	"github.com/canonical/chisel/internal/setup"
+	"github.com/canonical/chisel/internal/testutil"
+)
+
+type checkReleaseArchivesTest struct {
+	summary string
+	arch    string
+	release map[string]string
+	pkgs    []*testutil.TestPackage
+	stdout  string
+	err     string
+}
+
+var checkReleaseArchivesTests = []checkReleaseArchivesTest{{
+	summary: "No issue found",
+	release: map[string]string{
+		"chisel.yaml": makeChiselYaml([]string{"ubuntu"}),
+		"slices/mydir/pkg-a.yaml": `
+			package: pkg-a
+			slices:
+				myslice:
+					contents:
+		`,
+		"slices/mydir/pkg-b.yaml": `
+			package: pkg-b
+			slices:
+				myslice:
+					contents:
+		`,
+	},
+	pkgs: []*testutil.TestPackage{{
+		Name: "pkg-a",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0755, "./dir/"),
+		}),
+	}, {
+		Name: "pkg-b",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0755, "./dir/"),
+		}),
+	}},
+	stdout: "",
+}, {
+	summary: "All types of conflicts",
+	release: map[string]string{
+		"chisel.yaml": makeChiselYaml([]string{"ubuntu"}),
+		"slices/mydir/pkg-a.yaml": `
+			package: pkg-a
+			slices:
+				myslice:
+					contents:
+		`,
+		"slices/mydir/pkg-b.yaml": `
+			package: pkg-b
+			slices:
+				myslice:
+					contents:
+		`,
+	},
+	pkgs: []*testutil.TestPackage{{
+		Name: "pkg-a",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0755, "./mode/"),
+			testutil.Lnk(0777, "./link", "/other"),
+		}),
+	}, {
+		Name: "pkg-b",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0756, "./mode/"),
+			testutil.Dir(0777, "./link"),
+		}),
+	}},
+	stdout: `
+		- issue: path-conflict
+		  path: /link
+		  observations:
+			- archive: ubuntu
+			  packages: [pkg-a]
+			  kind: symlink
+			  link: /other
+			- archive: ubuntu
+			  packages: [pkg-b]
+			  kind: dir
+			  mode: 0777
+		- issue: path-conflict
+		  path: /mode
+		  observations:
+			- archive: ubuntu
+			  packages: [pkg-a]
+			  kind: dir
+			  mode: 0755
+			- archive: ubuntu
+			  packages: [pkg-b]
+			  kind: dir
+			  mode: 0756
+	`,
+	err: "issues found in the release archives",
+}, {
+	summary: "Multiple archives",
+	release: map[string]string{
+		"chisel.yaml": makeChiselYaml([]string{"archive1", "archive2"}),
+		"slices/mydir/pkg-a.yaml": `
+			package: pkg-a
+			slices:
+				myslice:
+					contents:
+		`,
+		"slices/mydir/pkg-b.yaml": `
+			package: pkg-b
+			slices:
+				myslice:
+					contents:
+		`,
+	},
+	pkgs: []*testutil.TestPackage{{
+		Name: "pkg-a",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0755, "./dir/"),
+		}),
+		Archives: []string{"archive1", "archive2"},
+	}, {
+		Name: "pkg-b",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0756, "./dir/"),
+		}),
+		Archives: []string{"archive2"},
+	}},
+	stdout: `
+		- issue: path-conflict
+		  path: /dir
+		  observations:
+			- archive: archive1
+			  packages: [pkg-a]
+			  kind: dir
+			  mode: 0755
+			- archive: archive2
+			  packages: [pkg-a]
+			  kind: dir
+			  mode: 0755
+			- archive: archive2
+			  packages: [pkg-b]
+			  kind: dir
+			  mode: 0756
+	`,
+	err: "issues found in the release archives",
+}, {
+	summary: "Parent directory conflict different arch",
+	release: map[string]string{
+		"chisel.yaml": makeChiselYaml([]string{"ubuntu"}),
+		"slices/mydir/pkg-a.yaml": `
+			package: pkg-a
+			slices:
+				myslice:
+					contents:
+		`,
+		"slices/mydir/pkg-b.yaml": `
+			package: pkg-b
+			slices:
+				myslice:
+					contents:
+		`,
+	},
+	arch: "arm64",
+	pkgs: []*testutil.TestPackage{{
+		Name: "pkg-a",
+		Arch: "arm64",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0755, "./dir/"),
+		}),
+	}, {
+		Name: "pkg-b",
+		Arch: "arm64",
+		Data: testutil.MustMakeDeb([]testutil.TarEntry{
+			testutil.Dir(0756, "./dir/"),
+		}),
+	}},
+	stdout: `
+		- issue: path-conflict
+		  path: /dir
+		  observations:
+			- archive: ubuntu
+			  packages: [pkg-a]
+			  kind: dir
+			  mode: 0755
+			- archive: ubuntu
+			  packages: [pkg-b]
+			  kind: dir
+			  mode: 0756
+	`,
+	err: "issues found in the release archives",
+}}
+
+func (s *ChiselSuite) TestRun(c *C) {
+	for _, test := range checkReleaseArchivesTests {
+		c.Logf("Summary: %s", test.summary)
+		s.ResetStdStreams()
+
+		releaseDir := c.MkDir()
+		for path, data := range test.release {
+			fpath := filepath.Join(releaseDir, path)
+			err := os.MkdirAll(filepath.Dir(fpath), 0755)
+			c.Assert(err, IsNil)
+			err = os.WriteFile(fpath, testutil.Reindent(data), 0644)
+			c.Assert(err, IsNil)
+		}
+
+		release, err := setup.ReadRelease(releaseDir)
+		c.Assert(err, IsNil)
+
+		archives := map[string]archive.Archive{}
+		for name, setupArchive := range release.Archives {
+			pkgs := make(map[string]*testutil.TestPackage)
+			for _, pkg := range test.pkgs {
+				if len(pkg.Archives) == 0 || slices.Contains(pkg.Archives, name) {
+					pkgs[pkg.Name] = pkg
+				}
+			}
+			archive := &testutil.TestArchive{
+				Opts: archive.Options{
+					Label:      setupArchive.Name,
+					Version:    setupArchive.Version,
+					Suites:     setupArchive.Suites,
+					Components: setupArchive.Components,
+					Pro:        setupArchive.Pro,
+					Arch:       test.arch,
+				},
+				Packages: pkgs,
+			}
+			archives[name] = archive
+		}
+
+		restore := chisel.FakeArchiveOpen(func(options *archive.Options) (archive.Archive, error) {
+			archive, ok := archives[options.Label]
+			c.Assert(ok, Equals, true)
+			c.Assert(archive.Options().Arch, Equals, options.Arch)
+			c.Assert(archive.Options().Pro, Equals, options.Pro)
+			c.Assert(archive.Options().Label, Equals, options.Label)
+			c.Assert(archive.Options().Version, Equals, options.Version)
+			c.Assert(archive.Options().Components, DeepEquals, options.Components)
+			c.Assert(archive.Options().Suites, DeepEquals, options.Suites)
+			return archive, nil
+		})
+		defer restore()
+
+		cliArgs := []string{"debug", "check-release-archives", "--release", releaseDir}
+		if test.arch != "" {
+			cliArgs = slices.Concat(cliArgs, []string{"--arch", test.arch})
+		}
+
+		_, err = chisel.Parser().ParseArgs(cliArgs)
+		if test.err != "" {
+			c.Assert(err, ErrorMatches, test.err)
+		} else {
+			c.Assert(err, IsNil)
+		}
+		if test.stdout != "" {
+			test.stdout = string(testutil.Reindent(test.stdout))
+			test.stdout = strings.TrimSpace(test.stdout) + "\n"
+		}
+		c.Assert(s.Stdout(), Equals, test.stdout)
+	}
+}
+
+// makeChiselYaml returns a valid chisel.yaml that contains the archives
+// supplied.
+func makeChiselYaml(archives []string) string {
+	archiveKey := testutil.PGPKeys["key-ubuntu-2018"]
+	rawChiselYaml := testutil.Reindent(`
+		format: v1
+		archives:
+			ubuntu:
+				version: 22.04
+				components: [main, universe]
+				suites: [jammy]
+				public-keys: [test-key]
+		public-keys:
+			test-key:
+				id: ` + archiveKey.ID + `
+				armor: |` + "\n" + testutil.PrefixEachLine(archiveKey.PubKeyArmor, "\t\t\t\t\t\t"))
+
+	chiselYaml := map[string]any{}
+	err := yaml.Unmarshal([]byte(rawChiselYaml), chiselYaml)
+	if err != nil {
+		panic(err)
+	}
+
+	archivesYaml := chiselYaml["archives"].(map[string]any)
+	// Use the ubuntuArchive as a "template".
+	ubuntuArchive := archivesYaml["ubuntu"].(map[string]any)
+	delete(archivesYaml, "ubuntu")
+
+	for i, archiveName := range archives {
+		archive := deepCopyYAML(ubuntuArchive)
+		// Valid chisel.yaml has different priorities.
+		archive["priority"] = i + 1
+		archivesYaml[archiveName] = archive
+	}
+
+	bs, err := yaml.Marshal(chiselYaml)
+	if err != nil {
+		panic(err)
+	}
+	return string(bs)
+}
+
+func deepCopyYAML(src map[string]any) map[string]any {
+	dest := map[string]any{}
+	for key, value := range src {
+		switch src[key].(type) {
+		case map[string]any:
+			dest[key] = map[string]any{}
+			dest[key] = deepCopyYAML(src[key].(map[string]any))
+		default:
+			dest[key] = value
+		}
+	}
+	return dest
+}
