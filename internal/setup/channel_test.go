9e512286d720fe06f933648d77cf55c954b30981
Paul Mars <paul.mars@canonical.com>
2026-07-31 11:07:52 +0200

    tests: remove superfluous test

--- internal/setup/channel_test.go
+++ internal/setup/channel_test.go
@@ -40,14 +40,3 @@ func (s *S) TestChannelString(c *C) {
 		c.Assert(test.channel.String(), Equals, test.expected)
 	}
 }
-
-// TestChannelStringRoundTrip ensures a parsed channel renders back to the
-// value it was parsed from, once the implicit risk is set as ParseSliceRef
-// does.
-func (s *S) TestChannelStringRoundTrip(c *C) {
-	for _, channel := range []string{"3.0/stable", "3.0/edge", "3.0/stable/mybranch"} {
-		ref, err := setup.ParseSliceRef("mypkg_myslice@" + channel)
-		c.Assert(err, IsNil)
-		c.Assert(ref.Channel.String(), Equals, channel)
-	}
-}
